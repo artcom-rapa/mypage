1197ec3b66abd9eb18022b79bc5cdedf2f3582d8
Ada Rapa <ada.rapa@interia.pl>
2020-06-02 06:09:51 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from flask import request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/me')
+def me():    return render_template("me.html")
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("contact.html")
+    elif request.method == 'POST':
+        print("We received POST: " + request.form['tresc'])
+        return request.form['tresc']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
